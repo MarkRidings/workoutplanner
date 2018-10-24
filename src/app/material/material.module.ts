fc9fe30c359561a303a0e2a97c174c368c4f4747
Mark Ridings <ridings.mark@gmail.com>
2018-10-24 06:59:00 -0400

    added Material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
+  ]
+})
+
+export class MaterialModule { }
